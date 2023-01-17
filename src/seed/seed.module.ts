52557fcc91ea60222cf4e11f8bec8c7316912460
pblgllgs <pbl.gllgs@gmail.com>
2023-01-17 17:41:44 -0300

    seed list and listItems

--- src/seed/seed.module.ts
+++ src/seed/seed.module.ts
@@ -4,10 +4,12 @@ import { SeedResolver } from './seed.resolver';
 import { UsersModule } from '../users/users.module';
 import { ItemsModule } from '../items/items.module';
 import { ConfigModule } from '@nestjs/config';
+import { ListModule } from '../list/list.module';
+import { ListItemModule } from '../list-item/list-item.module';
 
 @Module({
   providers: [SeedResolver, SeedService],
   exports: [SeedService],
-  imports: [UsersModule, ItemsModule, ConfigModule],
+  imports: [UsersModule, ItemsModule, ConfigModule, ListModule, ListItemModule],
 })
 export class SeedModule {}
--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,12 +1,16 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
 import { ConfigService } from '@nestjs/config';
+import { SEED_USERS, SEED_ITEMS, SEED_LIST } from './data/seed-data';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { User } from '../users/entities/user.entity';
 import { Item } from '../items/entities/item.entity';
-import { InjectRepository } from '@nestjs/typeorm';
+import { List } from '../list/entities/list.entity';
+import { ListItem } from '../list-item/entities/list-item.entity';
 import { UsersService } from '../users/users.service';
 import { ItemsService } from '../items/items.service';
-import { SEED_USERS, SEED_ITEMS } from './data/seed-data';
+import { ListItemService } from '../list-item/list-item.service';
+import { ListService } from '../list/list.service';
 
 @Injectable()
 export class SeedService {
@@ -20,6 +24,12 @@ export class SeedService {
     @InjectRepository(Item)
     private readonly itemRepository: Repository<Item>,
     private readonly itemService: ItemsService,
+    @InjectRepository(ListItem)
+    private readonly listItemRepository: Repository<ListItem>,
+    private readonly listItemService: ListItemService,
+    @InjectRepository(List)
+    private readonly listRepository: Repository<List>,
+    private readonly listService: ListService,
   ) {
     this.isProd = configService.get('STATE') === 'prod';
   }
@@ -31,10 +41,23 @@ export class SeedService {
     await this.deleteDatabase();
     const user = await this.loadUsers();
     await this.loadItems(user);
+    const list = await this.loadList(user);
+    const items = await this.itemService.findAll(
+      user,
+      { limit: 15, offset: 0 },
+      {},
+    );
+    await this.loadListItem(list, items);
     return true;
   }
 
   async deleteDatabase(): Promise<void> {
+    await this.listItemRepository
+      .createQueryBuilder()
+      .delete()
+      .where({})
+      .execute();
+    await this.listRepository.createQueryBuilder().delete().where({}).execute();
     await this.itemRepository.createQueryBuilder().delete().where({}).execute();
     await this.userRepository.createQueryBuilder().delete().where({}).execute();
   }
@@ -58,5 +81,24 @@ export class SeedService {
       );
     }
     await Promise.all(itemsPromises);
+  }
+
+  async loadList(user: User): Promise<List> {
+    const listPromises = [];
+    for (const list of SEED_LIST) {
+      listPromises.push(await this.listService.create(list, user));
+    }
+    return listPromises[0];
+  }
+
+  async loadListItem(list: List, items: Item[]): Promise<void> {
+    for (const item of items) {
+      this.listItemService.create({
+        quantity: Math.round(Math.random() * 10),
+        completed: Math.round(Math.random() * 1) === 0 ? false : true,
+        listId: list.id,
+        itemId: item.id,
+      });
+    }
   }
 }
